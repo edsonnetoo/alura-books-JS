d82d653738bbac5964cbfd587e9a98d09e398906
edsonnetoo <edson.creeper@gmail.com>
2025-01-01 16:36:21 -0300

    Aula 02 - Usando map

---
+++ app/metodoForEach.js
@@ -0,0 +1,20 @@
+const secaoLivros = document.getElementById("livros");
+
+function exibirLivros(listaLivros) {
+    listaLivros.forEach(livro => {
+        secaoLivros.innerHTML += `
+            <div class="livro">
+      <img class="livro__imagens" src="${livro.imagem}"
+        alt="${livro.alt}" />
+      <h2 class="livro__titulo">
+        ${livro.titulo}
+      </h2>
+      <p class="livro__descricao">${livro.autor}</p>
+      <p class="livro__preco" id="preco">R$ ${livro.preco.toFixed(2)}</p>
+      <div class="tags">
+        <span class="tag">${livro.categoria}</span>
+      </div>
+    </div>
+        `
+    });
+}
